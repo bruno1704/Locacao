36930d9ef264ebb7dd43d9a9e2151a625b2da09e
Arley Ferreira binda <arley.fee@hotmail.com>
2019-09-16 20:05:32 -0300

    ajuste login

--- Locacao/Locacao/Models/Usuario.cs
+++ Locacao/Locacao/Models/Usuario.cs
@@ -10,5 +10,6 @@ namespace Locacao.Models
         public int IdUsuario { get; set; }
         public string Senha { get; set; }
         public string Nome { get; set; }
+        public string Email { get; set; }
     }
 }
