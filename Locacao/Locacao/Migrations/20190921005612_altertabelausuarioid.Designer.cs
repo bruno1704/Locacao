7a2e52afd730ec696a8b5b4761102fdb66270161
Arley Ferreira binda <arley.fee@hotmail.com>
2019-09-22 14:40:39 -0300

    migration

--- Locacao/Locacao/Migrations/20190921005612_altertabelausuarioid.Designer.cs
+++ Locacao/Locacao/Migrations/20190921005612_altertabelausuarioid.Designer.cs
@@ -3,14 +3,16 @@ using Locacao.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Locacao.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20190921005612_altertabelausuarioid")]
+    partial class altertabelausuarioid
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Locacao/Locacao/Migrations/20190921005612_altertabelausuarioid.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Locacao.Migrations
+{
+    public partial class altertabelausuarioid : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IdUsuario",
+                table: "Usuario",
+                newName: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Usuario",
+                newName: "IdUsuario");
+        }
+    }
+}
