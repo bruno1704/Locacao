3a01690f336ae096a546da4c3f877ebee68a8fd0
Arley Ferreira binda <arley.fee@hotmail.com>
2019-09-22 14:38:53 -0300

    novos objs e relacionamento

--- Locacao/Locacao/Models/ApplicationContext.cs
+++ Locacao/Locacao/Models/ApplicationContext.cs
@@ -17,6 +17,24 @@ namespace Locacao.Models
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.Entity<Usuario>().HasKey(t => t.Id);
+            //modelBuilder.Entity<Usuario>().HasMany(t=>t.res)
+
+            modelBuilder.Entity<Veiculo>().HasKey(t => t.Id);
+
+            
+
+            modelBuilder.Entity<Reserva>().HasKey(t => t.Id);
+            modelBuilder.Entity<Reserva>().HasOne(t => t.usuario);
+            modelBuilder.Entity<Reserva>().HasOne(t => t.Veiculo);
+
+
+            modelBuilder.Entity<Pedido>().HasKey(t => t.Id);
+            modelBuilder.Entity<Pedido>().HasOne(t => t.Reserva);
+            modelBuilder.Entity<Pedido>().HasMany(t => t.Status).WithOne(t=>t.Pedido);
+
+            modelBuilder.Entity<StatusPedido>().HasKey(t => t.Id);
+            //modelBuilder.Entity<StatusPedido>().HasMany(t => t.Pedido);//.WithMany(t=>t.);
+
         }
     }
 }
